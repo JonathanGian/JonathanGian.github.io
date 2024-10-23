1da4155f453d59c5a0322b7a076c1bd1ecf9e24a
Jonathan Gian <jongian90@gmail.com>
2024-10-23 20:01:24 +0300

    Update Portfolio to final version

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,45 @@
+const backToTop = this.document.querySelector("#backtopBtn");
+const navList = document.querySelector("nav ul")
+const mobButton = document.querySelector(".mobile")
+
+function toggleMenu() {
+    navList.classList.toggle("responsive")
+}
+mobButton.addEventListener("click",toggleMenu)
+
 window.addEventListener('scroll', function() {
     const header = document.querySelector("nav");
     
     header.classList.toggle('scrolled', window.scrollY > 50);
+    backToTop.classList.toggle("hidden",window.scrollY < 50);
 });
 
+backToTop.addEventListener("click",() => {
+    document.body.scrollTop= 0;
+    document.documentElement.scrollTop = 0;
+    
+    })
+
+
+console.log(backToTop)
+
+
+// Script to adjust scroll when clicking links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const headerOffset = 60; 
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
